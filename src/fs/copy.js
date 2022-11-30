02959326095ec6932a256f6e196bfdf75b35b11d
Maxim Peshkur <1917217@gmail.com>
2022-11-30 15:02:23 +0300

    fs tasks

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,27 @@
+import { copyFile, constants, readdir, mkdir } from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
 const copy = async () => {
     // Write your code here 
+    readdir(__dirname + '/files', (error, files) => {
+        if (error) throw new Error('FS operation failed');
+
+        mkdir(__dirname + '/files_copy', (error, path) => {
+            if (error) throw new Error('FS operation failed');
+
+            files.forEach(fileName => {
+                copyFile(__dirname + '/files/' + fileName, __dirname + "/files_copy/" + fileName, constants.COPYFILE_EXCL, (error) => {
+                    if (error) throw new Error('FS operation failed');
+
+                })
+            });
+        })  
+    })
 };
 
 copy();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+
 const rename = async () => {
     // Write your code here 
+    
+    fs.rename(__dirname + '/files/wrongFilename.txt', __dirname + '/files/properFilename.md', (error) => {
+        if (error) throw new Error('FS operation failed');
+    })
 };
 
 await rename();
