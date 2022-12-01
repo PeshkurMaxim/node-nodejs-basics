5cf86392e0a895d845b539e052e73d445f48ee98
Maxim Peshkur <1917217@gmail.com>
2022-12-01 14:47:35 +0300

    cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
 const parseArgs = () => {
-    // Write your code here 
+    let resStr = '';
+
+    process.argv.slice(2).forEach( (arg, index) => {
+
+        if (arg.slice(0,2) === '--') {
+
+            const argFlag = arg.slice(2);
+            const argValue = process.argv[index + 3];
+            resStr += `${argFlag} is ${argValue}, `;
+
+        }
+    });
+
+    console.log(resStr.slice(0, -2));
 };
 
 parseArgs();
