db8a2d2e1933a0da541a1e80d48b958bcc0f7654
Maxim Peshkur <1917217@gmail.com>
2022-12-05 12:52:49 +0300

    cp, hash, wt tasks

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,19 @@
+import { fork } from 'child_process';
+import { dirname } from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+
+    const childProcess = fork(__dirname + '/files/script.js', process.argv.slice(2), {silent: true});
+
+    process.stdin.pipe(childProcess.stdin);
+
+    childProcess.stdout.pipe(process.stdout);
+
 };
 
 spawnChildProcess();
--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,51 @@
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+import { dirname } from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = dirname(__filename);
+
 const performCalculations = async () => {
-    // Write your code here
+
+    const arResult = [];
+
+    const workerResult = (data) => {
+        for (const workerResult of data) {
+            if (workerResult.status == 'fulfilled') {
+                arResult.push(workerResult.value);
+            }
+
+            if (workerResult.status == "rejected") {
+                arResult.push({
+                    status: 'error',
+                    data: null
+                });
+            }
+        }
+
+        console.log(arResult);
+    }
+
+    let promises = [];
+
+    for (let i = 0; i < cpus().length; i++) {
+
+        promises.push(
+            new Promise((resolve, reject) => {
+                const worker = new Worker(__dirname + '/worker.js', {workerData: i + 10});
+                worker.on('message', resolve);
+                worker.on('error', reject);
+                worker.on('exit', (code) => {
+                    if (code !== 0)
+                        reject(new Error(`Worker stopped with exit code ${code}`));
+                })     
+            })
+        )
+    }
+
+    Promise.allSettled(promises).then(workerResult);
 };
 
 await performCalculations();
